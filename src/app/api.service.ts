69f5dcc7fa3bea1e2131da986259e6ca238145e8
Kim Maida <kim.maida@auth0.com>
2018-09-13 16:32:58 -0400

    Simplify error handling in API service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { throwError, Observable } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { environment } from './../environments/environment';
 
@@ -9,15 +9,11 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getDragons$(): Observable<any[]> {
-    return this.http.get<any[]>(`${environment.auth.AUDIENCE}dragons`)
+    return this.http
+      .get<any[]>(`${environment.auth.AUDIENCE}dragons`)
       .pipe(
-        catchError(this._handleError)
+        catchError(err => throwError(err))
       );
-  }
-
-  private _handleError(err: HttpErrorResponse | any) {
-    const errorMsg = err.message || 'Unable to retrieve data';
-    return throwError(errorMsg);
   }
 
 }
